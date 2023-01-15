ec7cf55d2bc50aa6d3db6c529106148cc69e176e
@theshag1 <isloomoov@gmail.comm>
2023-01-15 18:54:03 +0300

    sssss

--- task2.py
+++ task2.py
@@ -2,4 +2,6 @@ with open('pupilation_country') as f:
     data = f.readlines()
     for i in range(1, len(data)):
         line = data[i]
-        if line.split(',')[2].split('"')[0]>200000:
+        people =  line.split(',')[2].split('"')[0]
+        people = int(people)
+        print(type(people))
