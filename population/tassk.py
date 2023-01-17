b9220e16e42518602a17b710580665dbd9d0bb4c
@theshag1 <isloomoov@gmail.comm>
2023-01-17 14:43:09 +0300

    not eror

---
+++ population/tassk.py
@@ -0,0 +1,18 @@
+def get_popilation():
+    import csv
+
+    with open('world_pupile.csv') as f:
+        lines = f.readlines()
+        result = []
+    for line in lines:
+        try:
+            name, count = line.split(',')[0].strip(), int(line.split(',')[2].strip())
+        except ValueError as e:
+            print(e)
+        else:
+            if count > 20000000:
+                result.append(f"{name} : {count}")
+    return result
+
+# not eror
+print(get_popilation())
