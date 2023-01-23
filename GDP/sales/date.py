9f8e5d504aa5c1683eb9e446c2a5a3631dab1db4
@theshag1 <isloomoov@gmail.comm>
2023-01-23 15:14:22 +0300

    2020 up date

--- GDP/sales/date.py
+++ GDP/sales/date.py
@@ -1,13 +1,15 @@
 import csv
 
 try:
-    with open('date.txt') as f:
+    with open('date.csv') as f:
         lines = csv.DictReader(f)
+
         for line in lines:
+            print(line)
             date = line.get('Order Date')
             product = line.get('Product')
             if date >= '04/10/19 ':
-                file = open('sale.txt', 'a')
+                file = open('sale.csv', 'w')
                 file.write(f"{product}:{date}\n")
                 file.close()
 except Exception as e:
