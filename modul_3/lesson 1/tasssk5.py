d45a451c8ff1f70b2f0f05256c6a41665aefbfa3
shagi <isloomoov@gmail.com>
2023-01-27 17:03:37 +0300

    lst only

---
+++ modul_3/lesson 1/tasssk5.py
@@ -0,0 +1,17 @@
+def get_sum(funk):
+    def sum_get(lst):
+        if type(lst) == list:
+            return funk(lst)
+        else:
+            print('input only lst !')
+
+    return sum_get
+
+
+@get_sum
+def all_get(lst):
+    return sum((i for i in range(len(lst))))
+
+
+lst = [1, 2, 4, 4,5]
+print(all_get(lst))
